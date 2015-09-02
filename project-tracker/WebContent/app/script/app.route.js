73b76a629008ba33d7b9a820ff8461534cf46b14
garth <garth.pidcock.ctr@navy.mil>
2015-09-02 14:42:46 -0400

    moved controller

--- project-tracker/WebContent/app/script/app.route.js
+++ project-tracker/WebContent/app/script/app.route.js
@@ -43,60 +43,7 @@
 		{
         	url:'/init',
     		templateUrl: 'app/init.html',
-    		controller: function($scope, $log, ResourceFactory, ProjectFactory) {
-    			
-    			$scope.initalizeResources = initResources;
-    			$scope.initalizeProjects = initProjects;
-
-    			function initProjects() {
-    				var defaultProjects = [];
-    				
-    				defaultProjects.push(getProject("NAVAIR", "NAVAIR", getResource("Garth", "Pidcock", "J", "MANAGER"), 1439006400000,1439006400000,1469937600000,-1,"GREEN"));
-    				
-    				ProjectFactory.initializeProjects(defaultProjects);
-    			}
-    			
-    			function initResources() {
-    				var defaultResources = [];
-        			defaultResources.push(getResource("Garth", "Pidcock", "J", "MANAGER"))
-        			defaultResources.push(getResource("Randy", "Bouquet", "", "MANAGER"))
-        			defaultResources.push(getResource("Kevin", "Criss", "", "MANAGER"))
-        			defaultResources.push(getResource("Bishwas", "Khanal", "", "MANAGER"))
-        			defaultResources.push(getResource("Brady", "Hustad", "", "MANAGER"))
-        			
-    				ResourceFactory.initializeResources(defaultResources);
-    			}
-    			
-    			function getResource(fn, ln, mi, role) {
-    				var rsc = {
-    					firstName: fn,
-    					lastName: ln,
-    					middleInitial: mi,
-    					role: role
-    				}
-    				
-    				return rsc;
-    			}
-    			
-    			function getProject(name, code, owner, pStart, aStart, pEnd, aEnd, status) {
-    				var json = {
-    					name : name,
-    					code : code,
-    					owner : owner,
-    					start : {
-    						projected : pStart,
-    						actual : aStart
-    					},
-    					end : {
-    						projected : pEnd,
-    						actual : aEnd
-    					},
-    					status : status
-    				};
-
-    				return json;
-    			}
-    		}
+    		controller: 'DataController'
 		});
 		
 	}]); 

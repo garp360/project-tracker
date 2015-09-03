3ab51b5379fdf84e13ae82ba10533d0e58a5a568
garp360 <garp360@gmail.com>
2015-09-03 07:53:27 -0400

    Allie commit

---
+++ project-tracker/WebContent/app/script/app.controller.js
@@ -0,0 +1,53 @@
+(function() {
+    'use strict';
+
+    angular
+    	.module('controller.module')
+    	.controller('AppController', AppController)
+    	.controller('SideBarLeftController', SideBarLeftController)
+    	.controller('SideBarRightController', SideBarRightController);
+    
+    	AppController.$inject = ['$scope', '$log', '$timeout', '$mdSidenav', '$mdUtil'];
+    	SideBarLeftController.$inject = ['$scope', '$log', '$timeout', '$mdSidenav'];
+    	SideBarRightController.$inject = ['$scope', '$log', '$timeout', '$mdSidenav'];
+    	
+    	function AppController ($scope, $log, $timeout, $mdSidenav, $mdUtil) 
+    	{
+    		$scope.toggleLeft = buildToggler('left');
+    	    $scope.toggleRight = buildToggler('right');
+    	    /**
+    	     * Build handler to open/close a SideNav; when animation finishes
+    	     * report completion in console
+    	     */
+    	    function buildToggler(navID) 
+    	    {
+    	      var debounceFn =  $mdUtil.debounce(function(){
+    	            $mdSidenav(navID)
+    	              .toggle()
+    	              .then(function () {
+    	                $log.debug("toggle " + navID + " is done");
+    	              });
+    	          },200);
+    	      return debounceFn;
+    	    }
+    	};
+    	
+    	function SideBarLeftController($scope, $log, $timeout, $mdSidenav) 
+    	{
+    	    $scope.close = function () {
+    	        $mdSidenav('left').close().then(function () {
+    	            $log.debug("close LEFT is done");
+    	        });
+    	    };
+    	};
+    	   
+    	    
+	    function SideBarRightController($scope, $log, $timeout, $mdSidenav) 
+	    {
+	    	$scope.close = function () {
+	    		$mdSidenav('right').close().then(function () {
+    				$log.debug("close RIGHT is done");
+    	        });
+	        };
+        };
+})();
